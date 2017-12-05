5e07fe190199110e508954be2087ac8538b67a2c
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-05 21:21:04 +0100

    Options added

--- src/GraphQL.Client/GraphQLClient.cs
+++ src/GraphQL.Client/GraphQLClient.cs
@@ -11,21 +11,30 @@ namespace GraphQL.Client {
 
 	public class GraphQLClient {
 
-		private const string GraphQLMediaType= "application/json"; // This should be application/graphql
-
 		public Uri EndPoint {
-			get { return this.httpClient.BaseAddress; }
-			set { this.httpClient.BaseAddress = value; }
+			get { return this.Options.EndPoint; }
+			set { this.Options.EndPoint = value; }
 		}
+
+		public GraphQLClientOptions Options { get; set; } = new GraphQLClientOptions();
 
 		private readonly HttpClient httpClient = new HttpClient();
 
-		public GraphQLClient(Uri endpoint) {
-			this.EndPoint = endpoint;
+		public GraphQLClient(Uri endPoint) {
+			this.Options.EndPoint = endPoint;
+		}
+
+		public GraphQLClient(GraphQLClientOptions options) {
+			this.Options = options;
+		}
+
+		public GraphQLClient(Uri endPoint, GraphQLClientOptions options) {
+			this.Options = options;
+			this.Options.EndPoint = endPoint;
 		}
 
 		public async Task<GraphQLResponse> GetQueryAsync(string query) {
-			var httpResponseMessage = await this.httpClient.GetAsync($"{this.httpClient.BaseAddress}?query={query}").ConfigureAwait(false);
+			var httpResponseMessage = await this.httpClient.GetAsync($"{this.Options.EndPoint}?query={query}").ConfigureAwait(false);
 			var resultString = await httpResponseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
 			return JsonConvert.DeserializeObject<GraphQLResponse>(resultString);
 		}
@@ -36,11 +45,11 @@ namespace GraphQL.Client {
 		}
 
 		public async Task<GraphQLResponse> PostAsync(GraphQLQuery query) {
-			var graphQLString = JsonConvert.SerializeObject(query,new JsonSerializerSettings {ContractResolver=new CamelCasePropertyNamesContractResolver() });
-			var httpContent = new StringContent(graphQLString, Encoding.UTF8, GraphQLMediaType);
-			var httpResponse=await this.httpClient.PostAsync(this.EndPoint, httpContent).ConfigureAwait(false);
+			var graphQLString = JsonConvert.SerializeObject(query, this.Options.JsonSerializerSettings);
+			var httpContent = new StringContent(graphQLString, Encoding.UTF8, this.Options.MediaType);
+			var httpResponse = await this.httpClient.PostAsync(this.EndPoint, httpContent).ConfigureAwait(false);
 			var resultString = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
-			return JsonConvert.DeserializeObject<GraphQLResponse>(resultString, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+			return JsonConvert.DeserializeObject<GraphQLResponse>(resultString, this.Options.JsonSerializerSettings);
 		}
 
 	}
--- src/GraphQL.Client/GraphQLClientOptions.cs
+++ src/GraphQL.Client/GraphQLClientOptions.cs
@@ -1,10 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace GraphQL.Client {
 
 	public class GraphQLClientOptions {
+
+		public Uri EndPoint { get; set; }
+
+		public JsonSerializerSettings JsonSerializerSettings { get; set; } = new JsonSerializerSettings {
+			ContractResolver = new CamelCasePropertyNamesContractResolver()
+		};
+
+		public string MediaType { get; set; } = "application/json"; // This should be "application/graphql"
 
 	}
 
