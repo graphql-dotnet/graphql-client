92560652092efa9a5f34022cbdf00c6765991623
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-07 02:19:16 +0100

    More documentation

--- src/GraphQL.Common/GrapQLQuery.cs
+++ src/GraphQL.Common/GrapQLQuery.cs
@@ -2,12 +2,24 @@ using Newtonsoft.Json.Linq;
 
 namespace GraphQL.Common {
 
+	/// <summary>
+	/// Represents a Query that can be fetched to a GraphQL Server
+	/// </summary>
 	public class GraphQLQuery {
 
+		/// <summary>
+		/// The Query
+		/// </summary>
 		public string Query { get; set; }
 
+		/// <summary>
+		/// The Name of the operation
+		/// </summary>
 		public string? OperationName { get; set; }
 
+		/// <summary>
+		/// The Variables
+		/// </summary>
 		public JObject? Variables { get; set; }
 
 	}
