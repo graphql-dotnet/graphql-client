f2229058298b2dce24ddfbe828271ef6a1049a07
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-08 23:31:33 +0100

    Moar Consts

--- tests/GraphQL.Common.Tests/Response/GraphQLResponseConsts.cs
+++ tests/GraphQL.Common.Tests/Response/GraphQLResponseConsts.cs
@@ -1,6 +1,85 @@
+using GraphQL.Common.Response;
+using Newtonsoft.Json;
+
 namespace GraphQL.Common.Tests.Response {
 
 	public class GraphQLResponseConsts {
+
+		/// <summary>
+		/// <see href="http://graphql.org/learn/queries/#fields"/>
+		/// </summary>
+		public static GraphQLResponse FieldsResponse1 { get; } = JsonConvert.DeserializeObject<GraphQLResponse>(@"
+			{
+				""data"": {
+					""hero"": {
+						""name"": ""R2-D2""
+					}
+				}
+			}");
+
+		/// <summary>
+		/// <see href="http://graphql.org/learn/queries/#fields"/>
+		/// </summary>
+		public static GraphQLResponse FieldsResponse2 { get; } = JsonConvert.DeserializeObject<GraphQLResponse>(@"
+			{
+				""data"": {
+					""hero"": {
+						""name"": ""R2-D2"",
+						""friends"": [
+							{
+								""name"": ""Luke Skywalker""
+							},
+							{
+								""name"": ""Han Solo""
+							},
+							{
+								""name"": ""Leia Organa""
+							}
+						]
+					}
+				}
+			}");
+
+		/// <summary>
+		/// <see href="http://graphql.org/learn/queries/#arguments"/>
+		/// </summary>
+		public static GraphQLResponse ArgumentsResponse1 { get; } = JsonConvert.DeserializeObject<GraphQLResponse>(@"
+			{
+				""data"": {
+					""human"": {
+						""name"": ""Luke Skywalker"",
+						""height"": 1.72
+					}
+				}
+			}");
+
+		/// <summary>
+		/// <see href="http://graphql.org/learn/queries/#arguments"/>
+		/// </summary>
+		public static GraphQLResponse ArgumentsResponse2 { get; } = JsonConvert.DeserializeObject<GraphQLResponse>(@"
+			{
+				""data"": {
+					""human"": {
+						""name"": ""Luke Skywalker"",
+						""height"": 5.6430448
+					}
+				}
+			}");
+
+		/// <summary>
+		/// <see href="http://graphql.org/learn/queries/#aliases"/>
+		/// </summary>
+		public static GraphQLResponse AliasesResponse { get; } = JsonConvert.DeserializeObject<GraphQLResponse>(@"
+			{
+				""data"": {
+					""empireHero"": {
+						""name"": ""Luke Skywalker""
+					},
+					""jediHero"": {
+						""name"": ""R2-D2""
+					}
+				}
+			}");
 
 	}
 
--- tests/GraphQL.Common.Tests/Response/GraphQLResponseTests.cs
+++ tests/GraphQL.Common.Tests/Response/GraphQLResponseTests.cs
@@ -1,6 +1,49 @@
+using Xunit;
+
 namespace GraphQL.Common.Tests.Response {
 
 	public class GraphQLResponseTests {
+
+		[Fact]
+		public void FieldsResponse1Fact() {
+			var graphQLResponse = GraphQLResponseConsts.FieldsResponse1;
+			AssertGraphQL.CorrectGraphQLResponse(graphQLResponse);
+			Assert.Equal("R2-D2", graphQLResponse.Data.hero.name.Value);
+		}
+
+		[Fact]
+		public void FieldsResponse2Fact() {
+			var graphQLResponse = GraphQLResponseConsts.FieldsResponse2;
+			AssertGraphQL.CorrectGraphQLResponse(graphQLResponse);
+			Assert.Equal("R2-D2", graphQLResponse.Data.hero.name.Value);
+			Assert.Equal("Luke Skywalker", graphQLResponse.Data.hero.friends[0].name.Value);
+			Assert.Equal("Han Solo", graphQLResponse.Data.hero.friends[1].name.Value);
+			Assert.Equal("Leia Organa", graphQLResponse.Data.hero.friends[2].name.Value);
+		}
+
+		[Fact]
+		public void ArgumentsResponse1Fact() {
+			var graphQLResponse = GraphQLResponseConsts.ArgumentsResponse1;
+			AssertGraphQL.CorrectGraphQLResponse(graphQLResponse);
+			Assert.Equal("Luke Skywalker", graphQLResponse.Data.human.name.Value);
+			Assert.Equal(1.72, graphQLResponse.Data.human.height.Value);
+		}
+
+		[Fact]
+		public void ArgumentsResponse2Fact() {
+			var graphQLResponse = GraphQLResponseConsts.ArgumentsResponse2;
+			AssertGraphQL.CorrectGraphQLResponse(graphQLResponse);
+			Assert.Equal("Luke Skywalker", graphQLResponse.Data.human.name.Value);
+			Assert.Equal(5.6430448, graphQLResponse.Data.human.height.Value);
+		}
+
+		[Fact]
+		public void AliasesResponseFact() {
+			var graphQLResponse = GraphQLResponseConsts.ArgumentsResponse2;
+			AssertGraphQL.CorrectGraphQLResponse(graphQLResponse);
+			Assert.Equal("Luke Skywalker", graphQLResponse.Data.empireHero.name.Value);
+			Assert.Equal("R2-D2", graphQLResponse.Data.jediHero.name.Value);
+		}
 
 	}
 
