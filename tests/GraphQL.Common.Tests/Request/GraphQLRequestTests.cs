38f8553db2ef264e481ffc268c2d35e6b949ea56
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-08 22:50:11 +0100

    all type of queries

--- tests/GraphQL.Common.Tests/Request/GraphQLRequestTests.cs
+++ tests/GraphQL.Common.Tests/Request/GraphQLRequestTests.cs
@@ -17,11 +17,86 @@ namespace GraphQL.Common.Tests.Request {
 		}
 
 		[Fact]
-		public void HeroNameAndFriendsRequestFact() {
-			var graphQLRequest = GraphQLRequestConsts.HeroNameAndFriendsRequest;
+		public void FieldsRequest1Fact() {
+			var graphQLRequest = GraphQLRequestConsts.FieldsRequest1;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void FieldsRequest2Fact() {
+			var graphQLRequest = GraphQLRequestConsts.FieldsRequest2;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void ArgumentsRequest1Fact() {
+			var graphQLRequest = GraphQLRequestConsts.ArgumentsRequest1;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void ArgumentsRequest2Fact() {
+			var graphQLRequest = GraphQLRequestConsts.ArgumentsRequest2;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void AliasesRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.AliasesRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void FragmentsRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.FragmentsRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void OperationNameRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.OperationNameRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+		}
+
+		[Fact]
+		public void VariablesRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.VariablesRequest;
 			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
 			Assert.NotNull(graphQLRequest.Variables);
 			Assert.Equal("JEDI", graphQLRequest.Variables.episode);
+		}
+
+		[Fact]
+		public void DirectivesRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.DirectivesRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+			Assert.NotNull(graphQLRequest.Variables);
+			Assert.Equal("JEDI", graphQLRequest.Variables.episode);
+			Assert.Equal(false, graphQLRequest.Variables.withFriends);
+		}
+
+		[Fact]
+		public void MutationsRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.MutationsRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+			Assert.NotNull(graphQLRequest.Variables);
+			Assert.Equal("JEDI", graphQLRequest.Variables.ep);
+			Assert.Equal(5, graphQLRequest.Variables.review.stars);
+			Assert.Equal("This is a great movie!", graphQLRequest.Variables.review.commentary);
+		}
+
+		[Fact]
+		public void InlineFragmentsRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.InlineFragmentsRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
+			Assert.NotNull(graphQLRequest.Variables);
+			Assert.Equal("JEDI", graphQLRequest.Variables.ep);
+		}
+
+		[Fact]
+		public void MetaFieldsRequestFact() {
+			var graphQLRequest = GraphQLRequestConsts.MetaFieldsRequest;
+			AssertGraphQL.CorrectGraphQLRequest(graphQLRequest);
 		}
 
 	}
