f32e18d68d74021bdad37f163c61c51222f1888e
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2017-12-09 22:45:19 +0100

    Exception done

--- src/GraphQL.Common/Exceptions/GraphQLException.cs
+++ src/GraphQL.Common/Exceptions/GraphQLException.cs
@@ -3,9 +3,23 @@ using GraphQL.Common.Response;
 
 namespace GraphQL.Common.Exceptions {
 
+	/// <summary>
+	/// An exception that contains a <see cref="Response.GraphQLError"/>
+	/// </summary>
 	public class GraphQLException : Exception {
 
-		public GraphQLError GraphQLError { get; set; }
+		/// <summary>
+		/// The GraphQLError
+		/// </summary>
+		public GraphQLError GraphQLError { get; }
+
+		/// <summary>
+		/// Constructor for a GraphQLException
+		/// </summary>
+		/// <param name="graphQLError">The GraphQL Error</param>
+		public GraphQLException(GraphQLError graphQLError):base(graphQLError.Message) {
+			this.GraphQLError = graphQLError;
+		}
 
 	}
 
