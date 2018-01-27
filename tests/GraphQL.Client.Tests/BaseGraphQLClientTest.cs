ba4ef8e04bd0d6eceb695552e52398942a83de8d
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-01-27 14:26:27 +0100

    protected

--- tests/GraphQL.Client.Tests/BaseGraphQLClientTest.cs
+++ tests/GraphQL.Client.Tests/BaseGraphQLClientTest.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace GraphQL.Client.Tests {
 
-	public abstract class BaseGraphQLClientTest : IDisposable{
+	public abstract class BaseGraphQLClientTest : IDisposable {
 
-		public GraphQLClient GraphQLClient { get; set; } = new GraphQLClient("https://swapi.apis.guru/");
+		protected GraphQLClient GraphQLClient { get; } = new GraphQLClient("https://swapi.apis.guru/");
 
 		public void Dispose() =>
 			this.GraphQLClient.Dispose();
