7a1b8f16d9b36f20e049d1db2a81e3121b2e19c1
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-03-26 11:25:18 +0200

    Subscription Result

---
+++ src/GraphQL.Client/Experimental/GraphQLSubscriptionResult.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace GraphQL.Client.Experimental {
+
+	/// <summary>
+	/// Represents the result of a Subscription Query
+	/// </summary>
+	[Obsolete("EXPERIMENTAL")]
+	public class GraphQLSubscriptionResult : IDisposable {
+
+		/// <summary>
+		/// Releases unmanaged resources
+		/// </summary>
+		public void Dispose() => throw new NotImplementedException();
+
+	}
+
+}
