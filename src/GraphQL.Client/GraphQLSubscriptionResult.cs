b5fa5e58106170b6e751c5ae5aeef994f279a101
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-05-19 23:44:32 +0200

    .

--- src/GraphQL.Client/GraphQLSubscriptionResult.cs
+++ src/GraphQL.Client/GraphQLSubscriptionResult.cs
@@ -1,7 +1,9 @@
 using System;
-using System.Collections.Generic;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
 using GraphQL.Common.Response;
+using Newtonsoft.Json;
 
 namespace GraphQL.Client {
 
@@ -9,15 +11,26 @@ namespace GraphQL.Client {
 	/// Represents the result of a subscription query
 	/// </summary>
 	[Obsolete("EXPERIMENTAL API")]
-	public class GraphQLSubscriptionResult {
+	public class GraphQLSubscriptionResult : IDisposable {
 
 		public event Action<GraphQLResponse> OnReceive;
 
 		public GraphQLResponse LastResponse { get; }
 
-		public GraphQLSubscriptionResult() {
-			this.OnReceive.Invoke(null);
+		private readonly ClientWebSocket clientWebSocket = new ClientWebSocket();
+
+		public async void StartAsync(CancellationToken cancellationToken = default) {
+			await this.clientWebSocket.ConnectAsync(new Uri("ws://localhost:5000/"), cancellationToken).ConfigureAwait(false);
+			var arraySegment = new ArraySegment<byte>(new byte[1024]);
+			while (this.clientWebSocket.State == WebSocketState.Open) {
+				var webSocketReceiveResult = await this.clientWebSocket.ReceiveAsync(arraySegment, cancellationToken).ConfigureAwait(false);
+				var stringResult = Encoding.UTF8.GetString(arraySegment.Array, 0, webSocketReceiveResult.Count);
+				var graphQLResponse = JsonConvert.DeserializeObject<GraphQLResponse>(stringResult);
+				if (graphQLResponse != null) { this.OnReceive.Invoke(graphQLResponse); }
+			}
 		}
+
+		public void Dispose() => this.clientWebSocket.Dispose();
 
 	}
 
