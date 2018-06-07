09bcc5da3ddb670e75afe13d956a45b621716e07
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-06-07 13:38:01 +0200

    .

---
+++ src/GraphQL.Client/GraphQLClient.Extensions.cs
@@ -0,0 +1,125 @@
+using System.Threading;
+using System.Threading.Tasks;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+
+namespace GraphQL.Client {
+
+	/// <summary>
+	/// Extension Methods for <see cref="GraphQLClient"/>
+	/// </summary>
+	public static class GraphQLClientExtensions {
+
+		private const string IntrospectionQuery = @"
+				query IntrospectionQuery {
+					__schema {
+						queryType {
+							name
+						},
+						mutationType {
+							name
+						},
+						subscriptionType {
+							name
+						},
+						types {
+							...FullType
+						},
+						directives {
+							name,
+							description,
+							args {
+								...InputValue
+							},
+							onOperation,
+							onFragment,
+							onField
+						}
+					}
+				}
+
+				fragment FullType on __Type {
+					kind,
+					name,
+					description,
+					fields(includeDeprecated: true) {
+						name,
+						description,
+						args {
+							...InputValue
+						},
+						type {
+							...TypeRef
+						},
+						isDeprecated,
+						deprecationReason
+					},
+					inputFields {
+						...InputValue
+					},
+					interfaces {
+						...TypeRef
+					},
+					enumValues(includeDeprecated: true) {
+						name,
+						description,
+						isDeprecated,
+						deprecationReason
+					},
+					possibleTypes {
+						...TypeRef
+					}
+				}
+
+				fragment InputValue on __InputValue {
+					name,
+					description,
+					type {
+						...TypeRef
+					},
+					defaultValue
+				}
+
+				fragment TypeRef on __Type {
+					kind,
+					name,
+					ofType {
+						kind,
+						name,
+						ofType {
+							kind,
+							name,
+							ofType {
+								kind,
+								name
+							}
+						}
+					}
+				}";
+
+		private static readonly GraphQLRequest IntrospectionGraphQLRequest = new GraphQLRequest {
+			Query = IntrospectionQuery.Replace("\t", "").Replace("\n", "").Replace("\r", ""),
+			Variables = null
+		};
+
+		/// <summary>
+		/// Send an IntrospectionQuery via GET
+		/// </summary>
+		/// <param name="graphQLClient">The GraphQLClient</param>
+		/// <param name="cancellationToken"></param>
+		/// <returns>The GraphQLResponse</returns>
+		public static async Task<GraphQLResponse> GetIntrospectionQueryAsync(this GraphQLClient graphQLClient, CancellationToken cancellationToken = default) =>
+			await graphQLClient.GetAsync(IntrospectionGraphQLRequest, cancellationToken).ConfigureAwait(false);
+
+		/// <summary>
+		/// Send an IntrospectionQuery via POST
+		/// </summary>
+		/// <param name="graphQLClient">The GraphQLClient</param>
+		/// <param name="cancellationToken"></param>
+		/// <returns>The GraphQLResponse</returns>
+		public static async Task<GraphQLResponse> PostIntrospectionQueryAsync(this GraphQLClient graphQLClient, CancellationToken cancellationToken = default) =>
+			await graphQLClient.PostAsync(IntrospectionGraphQLRequest, cancellationToken).ConfigureAwait(false);
+
+	}
+
+}
