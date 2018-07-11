defcf3ff2cc8c816c857bb2a61741c9a7f3a0853
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-07-11 21:18:55 +0200

    Documentation

--- src/GraphQL.Client/IGraphQLClient.cs
+++ src/GraphQL.Client/IGraphQLClient.cs
@@ -6,12 +6,34 @@ using GraphQL.Common.Response;
 
 namespace GraphQL.Client {
 
+	/// <summary>
+	/// Represents the Interface that any GraphQL Client must implement
+	/// </summary>
 	public interface IGraphQLClient : IDisposable {
 
+		/// <summary>
+		/// Send a Query async
+		/// </summary>
+		/// <param name="request">The Request</param>
+		/// <param name="cancellationToken">The Cancellation Token</param>
+		/// <returns>The Response</returns>
 		Task<GraphQLResponse> SendQueryAsync(GraphQLRequest request, CancellationToken cancellationToken = default);
 
+		/// <summary>
+		/// Send a Mutation Async
+		/// </summary>
+		/// <param name="request">The Request</param>
+		/// <param name="cancellationToken">The Cancellation Token</param>
+		/// <returns>The Response</returns>
 		Task<GraphQLResponse> SendMutationAsync(GraphQLRequest request, CancellationToken cancellationToken = default);
 
+		/// <summary>
+		/// Send a Subscription async
+		/// </summary>
+		/// <param name="request">The Request</param>
+		/// <param name="cancellationToken">The Cancellation Token</param>
+		/// <returns>The Subscription Response</returns>
+		[Obsolete("EXPERIMENTAL")]
 		Task<IGraphQLSubscriptionResult> SendSubscribeAsync(GraphQLRequest request, CancellationToken cancellationToken = default);
 
 	}
--- src/GraphQL.Client/IGraphQLSubscriptionResult.cs
+++ src/GraphQL.Client/IGraphQLSubscriptionResult.cs
@@ -3,10 +3,20 @@ using GraphQL.Common.Response;
 
 namespace GraphQL.Client {
 
+	/// <summary>
+	/// Represents the Result of a subscription
+	/// </summary>
+	[Obsolete("EXPERIMENTAL")]
 	public interface IGraphQLSubscriptionResult : IDisposable {
 
+		/// <summary>
+		/// Event triggered when a new Response is received
+		/// </summary>
 		event Action<GraphQLResponse> OnReceive;
 
+		/// <summary>
+		/// Last Response Received
+		/// </summary>
 		GraphQLResponse LastResponse { get; }
 
 	}
