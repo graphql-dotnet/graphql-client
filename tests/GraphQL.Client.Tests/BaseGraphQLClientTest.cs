53489f622dc34d2af93fd6d8469bce8db9218e2c
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-08-08 21:06:59 +0200

    IGraphQLClientTest

--- tests/GraphQL.Client.Tests/BaseGraphQLClientTest.cs
+++ tests/GraphQL.Client.Tests/BaseGraphQLClientTest.cs
@@ -6,10 +6,12 @@ namespace GraphQL.Client.Tests {
 	public abstract class BaseGraphQLClientTest : IDisposable {
 
 		protected GraphQLClient GraphQLClient { get; } = new GraphQLClient("https://swapi.apis.guru/");
+		protected IGraphQLClient GraphQLClientSwapi { get; } = new GraphQLHttpClient("https://swapi.apis.guru/");
 		protected GraphQLHttpClient GitHuntClient { get; } = new GraphQLHttpClient("http://api.githunt.com/graphql");
 
 		public void Dispose() {
 			this.GraphQLClient.Dispose();
+			this.GraphQLClientSwapi.Dispose();
 			this.GitHuntClient.Dispose();
 		}
 
---
+++ tests/GraphQL.Client.Tests/IGraphQLClientTest.cs
@@ -0,0 +1,95 @@
+using GraphQL.Common.Request;
+using GraphQL.Common.Tests.Model;
+using Xunit;
+
+namespace GraphQL.Client.Tests {
+
+	public class IGraphQLClientTest : BaseGraphQLClientTest {
+
+		[Fact]
+		public async void QueryGetAsyncFact() {
+			var graphQLRequest = new GraphQLRequest {
+				Query = @"
+				{
+					person(personID: ""1"") {
+						name
+					}
+				}"
+			};
+			var response = await this.GraphQLClientSwapi.SendQueryAsync(graphQLRequest).ConfigureAwait(false);
+
+			Assert.Equal("Luke Skywalker", response.Data.person.name.Value);
+			Assert.Equal("Luke Skywalker", response.GetDataFieldAs<Person>("person").Name);
+		}
+
+		[Fact]
+		public async void OperationNameGetAsyncFact() {
+			var graphQLRequest = new GraphQLRequest {
+				Query = @"
+				query Person{
+					person(personID: ""1"") {
+						name
+					}
+				}
+
+				query Planet {
+					planet(planetID: ""1"") {
+						name
+					}
+				}",
+				OperationName = "Person"
+			};
+			var response = await this.GraphQLClientSwapi.SendQueryAsync(graphQLRequest).ConfigureAwait(false);
+
+			Assert.Equal("Luke Skywalker", response.Data.person.name.Value);
+			Assert.Equal("Luke Skywalker", response.GetDataFieldAs<Person>("person").Name);
+		}
+
+		[Fact]
+		public async void VariablesGetAsyncFact() {
+			var graphQLRequest = new GraphQLRequest {
+				Query = @"
+				query Person($personId: ID!){
+					person(personID: $personId) {
+						name
+					}
+				}",
+				Variables = new {
+					personId = "1"
+				}
+			};
+			var response = await this.GraphQLClientSwapi.SendQueryAsync(graphQLRequest).ConfigureAwait(false);
+
+			Assert.Equal("Luke Skywalker", response.Data.person.name.Value);
+			Assert.Equal("Luke Skywalker", response.GetDataFieldAs<Person>("person").Name);
+		}
+
+		[Fact]
+		public async void OperationNameVariableGetAsyncFact() {
+			var graphQLRequest = new GraphQLRequest {
+				Query = @"
+				query Person($personId: ID!){
+					person(personID: $personId) {
+						name
+					}
+				}
+
+				query Planet {
+					planet(planetID: ""1"") {
+						name
+					}
+				}",
+				OperationName = "Person",
+				Variables = new {
+					personId = "1"
+				}
+			};
+			var response = await this.GraphQLClientSwapi.SendQueryAsync(graphQLRequest).ConfigureAwait(false);
+
+			Assert.Equal("Luke Skywalker", response.Data.person.name.Value);
+			Assert.Equal("Luke Skywalker", response.GetDataFieldAs<Person>("person").Name);
+		}
+
+	}
+
+}
