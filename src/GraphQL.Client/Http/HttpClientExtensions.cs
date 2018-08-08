d1c1671310c02a7354d1ff9722ae0bf6c6491176
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-08-08 22:13:28 +0200

    Internal Use As GraphQL

---
+++ src/GraphQL.Client/Http/HttpClientExtensions.cs
@@ -0,0 +1,21 @@
+using System.Net.Http;
+
+namespace GraphQL.Client.Http {
+
+	/// <summary>
+	/// Extensions for <see cref="HttpClient"/>
+	/// </summary>
+	public static class HttpClientExtensions {
+
+		/// <summary>
+		/// Creates a <see cref="GraphQLHttpClient"/> from a <see cref="HttpClient"/>
+		/// </summary>
+		/// <param name="httpClient"></param>
+		/// <param name="graphQLHttpClientOptions"></param>
+		/// <returns></returns>
+		public static GraphQLHttpClient AsGraphQLClient(this HttpClient httpClient, GraphQLHttpClientOptions graphQLHttpClientOptions) =>
+			new GraphQLHttpClient(graphQLHttpClientOptions, httpClient);
+
+	}
+
+}
