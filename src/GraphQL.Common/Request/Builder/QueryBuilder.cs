3c240484d8c507a6ab588d444cf365e9ccf2c6c4
Greg Dennis <gregsdennis@yahoo.com>
2019-01-31 22:24:26 +1300

    QueryBuilder - complex, nested graph support

--- src/GraphQL.Common/Request/Builder/QueryBuilder.cs
+++ src/GraphQL.Common/Request/Builder/QueryBuilder.cs
@@ -2,49 +2,58 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using Humanizer;
 
 namespace GraphQL.Common.Request.Builder
 {
-	public class QueryBuilder<TEntity>
+	public interface IQueryBuilder
 	{
-		private readonly List<QueryBuilder<TEntity>> _fields = new List<QueryBuilder<TEntity>>();
-		private readonly List<QueryParameter> _parameters = new List<QueryParameter>();
-		private string _name;
+		string Name { get; set; }
+		IQueryBuilder Parent { get; set; }
+		IQueryBuilder CurrentField { get; set; }
+		string Build();
+		IQueryBuilder TryAddField(IQueryBuilder field);
+	}
 
-		public string Name
+	public interface IQueryBuilder<out TEntity> : IQueryBuilder
+	{
+
+	}
+
+	public interface IQueryBuilder<out TEntity, out TProp> : IQueryBuilder<TEntity>
+	{
+
+	}
+
+	public abstract class QueryBuilder : IQueryBuilder
+	{
+		protected readonly List<IQueryBuilder> _fields = new List<IQueryBuilder>();
+
+		public string Name { get; set; }
+
+		public IQueryBuilder Parent { get; set; }
+
+		public IQueryBuilder CurrentField { get; set; }
+
+		public QueryBuilder()
 		{
-			get => _name ?? (_name = typeof(TEntity).Name);
-			set => _name = value;
+
 		}
 
-		public QueryBuilder<TEntity> Root { get; set; }
-
-		public QueryBuilder<TEntity, TProp> Include<TProp>(Expression<Func<TEntity, TProp>> propertyExpression)
+		protected QueryBuilder(QueryBuilder source)
 		{
-			var body = propertyExpression.Body as MemberExpression;
-			if (body == null)
-				throw new ArgumentException("Expression must indicate a property.");
-			var propertyName = body.Member.Name;
-
-			var field = new QueryBuilder<TEntity, TProp>
-			{
-				Name = propertyName,
-				Root = this
-			};
-
-			_fields.Add(field);
-
-			return field;
+			_fields = new List<IQueryBuilder>(source._fields);
+			CurrentField = source.CurrentField;
 		}
 
 		public string Build()
 		{
-			QueryBuilder<TEntity> root = Root;
-			while (root?.Root != null)
+			var root = Parent;
+			while (root?.Parent != null)
 			{
-				root = root.Root;
+				root = root.Parent;
 			}
 
 			return root?.ToString() ?? ToString();
@@ -58,9 +67,105 @@ namespace GraphQL.Common.Request.Builder
 			return stringBuilder.ToString();
 		}
 
-		private void Build(StringBuilder stringBuilder, int nestLevel)
+		public abstract void Build(StringBuilder stringBuilder, int nestLevel);
+
+		public IQueryBuilder TryAddField(IQueryBuilder field)
+		{
+			// TODO maybe replace with dictionary
+			var found = _fields.FirstOrDefault(f => f.Name == field.Name);
+			if (found == null)
+			{
+				_fields.Add(field);
+				found = field;
+			}
+
+			return found;
+		}
+	}
+
+	public static class QueryBuilderExtensions
+	{
+		public static IQueryBuilder<TEntity, TProp> Include<TEntity, TProp>(this IQueryBuilder<TEntity> builder, Expression<Func<TEntity, TProp>> propertyExpression)
+		{
+			var body = propertyExpression.Body as MemberExpression;
+			var property = body?.Member as PropertyInfo;
+			if (property == null)
+				throw new ArgumentException("Expression must indicate a property.", nameof(propertyExpression));
+			var propertyName = body.Member.Name;
+
+			var newBuilder = new QueryBuilder<TEntity, TProp>((QueryBuilder<TEntity>) builder);
+			IQueryBuilder field = new QueryBuilder<TEntity, TProp>
+			{
+				Name = propertyName,
+				Parent = newBuilder
+			};
+
+			field = newBuilder.TryAddField(field);
+			newBuilder.CurrentField = field;
+
+			return newBuilder;
+		}
+		public static IQueryBuilder<TEntity, TProp> ThenInclude<TEntity, TProp, TChild>(this IQueryBuilder<TEntity, TProp> builder, Expression<Func<TProp, TChild>> propertyExpression)
+		{
+			var body = propertyExpression.Body as MemberExpression;
+			var property = body?.Member as PropertyInfo;
+			if (property == null)
+				throw new ArgumentException("Expression must indicate a property.", nameof(propertyExpression));
+			var propertyName = body.Member.Name;
+
+			var newBuilder = new QueryBuilder<TEntity, TProp>((QueryBuilder<TEntity>) builder);
+			IQueryBuilder field = new QueryBuilder<TEntity, TProp>
+			{
+				Name = propertyName,
+				Parent = newBuilder
+			};
+
+			field = newBuilder.CurrentField.TryAddField(field);
+			newBuilder.CurrentField = field;
+
+			return newBuilder;
+		}
+		public static IQueryBuilder<TEntity, TChild> ThenInclude<TEntity, TProp, TChild>(this IQueryBuilder<TEntity, IEnumerable<TProp>> builder, Expression<Func<TProp, TChild>> propertyExpression)
+		{
+			var body = propertyExpression.Body as MemberExpression;
+			var property = body?.Member as PropertyInfo;
+			if (property == null)
+				throw new ArgumentException("Expression must indicate a property.", nameof(propertyExpression));
+			var propertyName = body.Member.Name;
+
+			var newBuilder = new QueryBuilder<TEntity, TChild>((QueryBuilder<TEntity>) builder);
+			IQueryBuilder field = new QueryBuilder<TEntity, TChild>
+			{
+				Name = propertyName,
+				Parent = builder
+			};
+
+			field = newBuilder.CurrentField.TryAddField(field);
+			newBuilder.CurrentField = field;
+
+			return newBuilder;
+		}
+	}
+
+	public class QueryBuilder<TEntity> : QueryBuilder, IQueryBuilder<TEntity>
+	{
+		private readonly List<QueryParameter> _parameters = new List<QueryParameter>();
+	
+		public QueryBuilder()
+		{
+			Name = typeof(TEntity).Name;
+		}
+
+		protected QueryBuilder(QueryBuilder<TEntity> source)
+			: base(source)
+		{
+			Name = typeof(TEntity).Name;
+		}
+
+		public override void Build(StringBuilder stringBuilder, int nestLevel)
 		{
 			const string space = "  ";
+
 			var tab = string.Join(string.Empty, Enumerable.Repeat(space, nestLevel));
 			nestLevel++;
 
@@ -68,11 +173,11 @@ namespace GraphQL.Common.Request.Builder
 			if (_fields.Any())
 			{
 				stringBuilder.AppendLine(" {");
-				foreach (var field in _fields)
+				foreach (QueryBuilder field in _fields)
 				{
 					field.Build(stringBuilder, nestLevel);
 				}
-				stringBuilder.Append("}");
+				stringBuilder.Append($"{tab}}}");
 			}
 
 			stringBuilder.AppendLine();
@@ -84,16 +189,15 @@ namespace GraphQL.Common.Request.Builder
 
 	}
 
-	public class QueryBuilder<TEntity, TProp> : QueryBuilder<TEntity>
+	public class QueryBuilder<TEntity, TProp> : QueryBuilder<TEntity>, IQueryBuilder<TEntity, TProp>
 	{
-		private List<QueryBuilder<TProp>> _children;
-
-		public List<QueryBuilder<TProp>> Children
+		public QueryBuilder()
 		{
-			get => _children ?? (_children = new List<QueryBuilder<TProp>>());
-			set => _children = value;
 		}
 
-		internal QueryBuilder() { }
+		internal QueryBuilder(QueryBuilder<TEntity> source)
+			: base(source)
+		{
+		}
 	}
 }
--- tests/GraphQL.Common.Tests/Request/Builder/QueryBuilderTests.cs
+++ tests/GraphQL.Common.Tests/Request/Builder/QueryBuilderTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using GraphQL.Common.Request.Builder;
 using Xunit;
 
@@ -11,6 +10,7 @@ namespace GraphQL.Common.Tests.Request.Builder
 		private class Human
 		{
 			public string Name { get; set; }
+			public int Age { get; set; }
 			public List<Human> Friends { get; set; }
 		}
 
@@ -23,6 +23,75 @@ namespace GraphQL.Common.Tests.Request.Builder
 ";
 
 			var actual = new QueryBuilder<Human>()
+				.Include(h => h.Name)
+				.Build();
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void BuildSimpleHumanUsingNonProperty()
+		{
+			var ex = Assert.Throws<ArgumentException>(() =>
+			{
+				new QueryBuilder<Human>()
+					.Include(h => h.ToString())
+					.Build();
+			});
+
+			Assert.StartsWith("Expression must indicate a property.", ex.Message);
+		}
+
+		[Fact]
+		public void BuildSimpleHumanWithFriends()
+		{
+			var expected = @"human {
+  friends {
+    name
+    age
+  }
+  name
+}
+";
+
+			var actual = new QueryBuilder<Human>()
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Name)
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Age)
+				.Include(h => h.Name)
+				.Build();
+
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void BuildSimpleHumanWithFriendsOfFriends()
+		{
+			var expected = @"human {
+  friends {
+    name
+    age
+    friends {
+      name
+      age
+    }
+  }
+  name
+}
+";
+
+			var actual = new QueryBuilder<Human>()
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Name)
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Age)
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Friends)
+				.ThenInclude(f => f.Name)
+				.Include(h => h.Friends)
+				.ThenInclude(f => f.Friends)
+				.ThenInclude(f => f.Age)
 				.Include(h => h.Name)
 				.Build();
 
