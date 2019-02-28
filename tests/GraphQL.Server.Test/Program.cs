f71091de545f5c0db735321f7b6ee6c18febb887
Deinok <raul.hidalgo.sopa@gmail.com>
2019-02-28 19:55:22 +0100

    reduced

--- tests/GraphQL.Server.Test/Program.cs
+++ tests/GraphQL.Server.Test/Program.cs
@@ -1,28 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace GraphQL.Server.Test
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace GraphQL.Server.Test {
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+	public class Program {
+
+		public static async Task Main(string[] args) => await CreateHostBuilder(args).Build().RunAsync();
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+                .ConfigureWebHostDefaults(webBuilder =>{
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
+
 }
