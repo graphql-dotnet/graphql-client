76c67b78d685d4054486d3fff312a5ef5e97c51c
Deinok <raul.hidalgo.sopa@gmail.com>
2019-03-02 19:20:25 +0100

    Update People.cs

--- tests/GraphQL.Server.Test/GraphQL/Models/People.cs
+++ tests/GraphQL.Server.Test/GraphQL/Models/People.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections.Generic;
 using GraphQL.Types;
 
 namespace GraphQL.Server.Test.GraphQL.Models {
 
 	public class People {
 
+		public string BirthYear { get; set; }
+
+		public DateTime Created { get; set; }
+
+		public DateTime Edited { get; set; }
+
+		public string EyeColor { get; set; }
+
+		public List<Film> Films { get; set; }
+
+		public string Gender { get; set; }
+
+		public string HairColor { get; set; }
+
 		public int Height { get; set; }
+
+		public Planet Homeworld { get; set; }
+
+		public int Id { get; set; }
 
 		public int Mass { get; set; }
 
 		public string Name { get; set; }
+
+		public string SkinColor { get; set; }
+
+		public List<Specie> Species { get; set; }
+
+		public List<Starship> Starships { get; set; }
+
+		public List<Vehicle> Vehicles { get; set; }
 
 	}
 
@@ -16,9 +44,22 @@ namespace GraphQL.Server.Test.GraphQL.Models {
 
 		public PeopleGraphType() {
 			this.Name = nameof(People);
+			this.Field(expression => expression.BirthYear);
+			this.Field(expression => expression.Created);
+			this.Field(expression => expression.Edited);
+			this.Field(expression => expression.EyeColor);
+			this.Field(expression => expression.Films);
+			this.Field(expression => expression.Gender);
+			this.Field(expression => expression.HairColor);
 			this.Field(expression => expression.Height);
+			this.Field(expression => expression.Homeworld);
+			this.Field(expression => expression.Id);
 			this.Field(expression => expression.Mass);
 			this.Field(expression => expression.Name);
+			this.Field(expression => expression.SkinColor);
+			this.Field(expression => expression.Species);
+			this.Field(expression => expression.Starships);
+			this.Field(expression => expression.Vehicles);
 		}
 
 	}
