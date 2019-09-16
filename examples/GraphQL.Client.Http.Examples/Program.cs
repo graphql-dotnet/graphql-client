3ac39126c08cf00c0b36db77d30d320e4b6ed466
deinok <deinok@deinok.com>
2019-09-16 04:19:05 +0200

    Example

---
+++ examples/GraphQL.Client.Http.Examples/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Threading.Tasks;
+
+namespace GraphQL.Client.Http.Examples {
+
+	public class Program {
+
+		public async static Task Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+
+	}
+
+}
