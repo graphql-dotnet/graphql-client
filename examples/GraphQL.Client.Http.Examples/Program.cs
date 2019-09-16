79ad031761408c55992e49737cf0c704aa18004b
deinok <deinok@deinok.com>
2019-09-16 07:27:05 +0200

    example

--- examples/GraphQL.Client.Http.Examples/Program.cs
+++ examples/GraphQL.Client.Http.Examples/Program.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
 
 namespace GraphQL.Client.Http.Examples {
 
 	public class Program {
 
+		private static readonly TestServer testServer = new TestServer(Server.Test.Program.CreateHostBuilder());
+
 		public async static Task Main(string[] args) {
+			var httpClient = testServer.CreateWebSocketClient();
 			Console.WriteLine("Hello World!");
 		}
 
