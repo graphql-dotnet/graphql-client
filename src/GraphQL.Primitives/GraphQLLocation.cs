3b43c8116030e9a3d5fa9d8f19470cf468424d07
deinok <deinok@deinok.com>
2019-10-06 19:54:45 +0200

    GraphQL.Primitives

--- src/GraphQL.Primitives/GraphQLLocation.cs
+++ src/GraphQL.Primitives/GraphQLLocation.cs
@@ -3,30 +3,65 @@ using System.Collections.Generic;
 
 namespace GraphQL {
 
+	/// <summary>
+	/// Represents a GraphQL Location of a GraphQL Query
+	/// </summary>
 	public class GraphQLLocation : IEquatable<GraphQLLocation?> {
 
+		/// <summary>
+		/// The Column
+		/// </summary>
 		public uint Column { get; set; }
 
+		/// <summary>
+		/// The Line
+		/// </summary>
 		public uint Line { get; set; }
 
+		/// <summary>
+		/// Returns a value that indicates whether this instance is equal to a specified object
+		/// </summary>
+		/// <param name="obj">The object to compare with this instance</param>
+		/// <returns>true if obj is an instance of <see cref="GraphQLLocation"/> and equals the value of the instance; otherwise, false</returns>
 		public override bool Equals(object obj) {
 			var graphQLLocation = obj as GraphQLLocation;
 			return graphQLLocation != null &&
 				this.Equals(graphQLLocation);
 		}
 
+		/// <summary>
+		/// Returns a value that indicates whether this instance is equal to a specified object
+		/// </summary>
+		/// <param name="other">The object to compare with this instance</param>
+		/// <returns>true if obj is an instance of <see cref="GraphQLLocation"/> and equals the value of the instance; otherwise, false</returns>
 		public bool Equals(GraphQLLocation? other) {
 			return other != null &&
 				EqualityComparer<uint>.Default.Equals(this.Column, other.Column) &&
 				EqualityComparer<uint>.Default.Equals(this.Line, other.Line);
 		}
 
+		/// <summary>
+		/// Returns the hash code for this instance
+		/// </summary>
+		/// <returns>The hash code</returns>
 		public override int GetHashCode() =>
 			this.Column.GetHashCode() ^ this.Line.GetHashCode();
 
+		/// <summary>
+		/// Tests whether two specified <see cref="GraphQLLocation"/> instances are equivalent
+		/// </summary>
+		/// <param name="left">The <see cref="GraphQLLocation"/> instance that is to the left of the equality operator</param>
+		/// <param name="right">The <see cref="GraphQLLocation"/> instance that is to the right of the equality operator</param>
+		/// <returns>true if left and right are equal; otherwise, false</returns>
 		public static bool operator ==(GraphQLLocation? left, GraphQLLocation? right) =>
 			EqualityComparer<GraphQLLocation?>.Default.Equals(left, right);
 
+		/// <summary>
+		/// Tests whether two specified <see cref="GraphQLLocation"/> instances are not equal
+		/// </summary>
+		/// <param name="left">The <see cref="GraphQLLocation"/> instance that is to the left of the not equal operator</param>
+		/// <param name="right">The <see cref="GraphQLLocation"/> instance that is to the right of the not equal operator</param>
+		/// <returns>true if left and right are unequal; otherwise, false</returns>
 		public static bool operator !=(GraphQLLocation? left, GraphQLLocation? right) =>
 			!EqualityComparer<GraphQLLocation?>.Default.Equals(left, right);
 
