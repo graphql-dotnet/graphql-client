94213e53a3aec013e53f1b13216eec6891691124
Alexander Rose <alex@rose-a.de>
2020-04-16 22:39:46 +0200

    remove orphaned file

--- src/GraphQL.Client.Http/Websocket/IGraphQLWebSocketJsonSerializer.cs
+++
@@ -1,5 +0,0 @@
-namespace GraphQL.Client.Http.Websocket {
-	public interface IGraphQLWebSocketJsonSerializer: IGraphQLJsonSerializer {
-		GraphQLWebSocketResponse<TResponse> DeserializeWebSocketResponse<TResponse>(byte[] utf8bytes);
-	}
-}
