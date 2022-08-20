2644067f9ebc7cbdb5feacb9105a6457e3d10da6
Ivan Maximov <sungam3r@yandex.ru>
2022-08-20 09:48:05 +0300

    Update tests/GraphQL.Integration.Tests/QueryAndMutationTests/Base.cs

--- tests/GraphQL.Integration.Tests/QueryAndMutationTests/Base.cs
+++ tests/GraphQL.Integration.Tests/QueryAndMutationTests/Base.cs
@@ -153,9 +153,9 @@ public abstract class Base : IAsyncLifetime
         });
 
         var defaultHeaders = StarWarsClient.HttpClient.DefaultRequestHeaders;
-        var userAgentOption = StarWarsClient.Options.DefaultUserAgentRequestHeader;
-        if (userAgentOption != null)
-            defaultHeaders.UserAgent.Add(userAgentOption(StarWarsClient.Options));
+        var userAgent = StarWarsClient.Options.DefaultUserAgentRequestHeader;
+        if (userAgent != null)
+            defaultHeaders.UserAgent.Add(userAgent);
         var response = await StarWarsClient.SendQueryAsync(graphQLRequest, () => new { Human = new { Name = string.Empty } });
         callbackTester.Should().HaveBeenInvokedWithPayload().Which.Headers.Should().BeEquivalentTo(defaultHeaders);
         Assert.Null(response.Errors);
