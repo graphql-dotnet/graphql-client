9f842be1004d434427186988e5ab6e082617f79b
Ivan Maximov <sungam3r@yandex.ru>
2023-04-24 19:59:00 +0300

    Add APQ support

--- src/GraphQL.Client/GraphQLHttpClient.cs
+++ src/GraphQL.Client/GraphQLHttpClient.cs
@@ -19,6 +19,11 @@ public class GraphQLHttpClient : IGraphQLWebSocketClient, IDisposable
     private readonly bool _disposeHttpClient = false;
 
     /// <summary>
+    /// This flag is used to completely disable APQ when GraphQL server does not support it.
+    /// </summary>
+    private bool _useAPQ = true;
+
+    /// <summary>
     /// the json serializer
     /// </summary>
     public IGraphQLWebsocketJsonSerializer JsonSerializer { get; }
@@ -89,7 +94,7 @@ public class GraphQLHttpClient : IGraphQLWebSocketClient, IDisposable
     {
         return Options.UseWebSocketForQueriesAndMutations || Options.WebSocketEndPoint is not null && Options.EndPoint is null || Options.EndPoint.HasWebSocketScheme()
             ? await GraphQlHttpWebSocket.SendRequestAsync<TResponse>(request, cancellationToken).ConfigureAwait(false)
-            : await SendHttpRequestAsync<TResponse>(request, cancellationToken).ConfigureAwait(false);
+            : await SendAPQHttpRequestAsync<TResponse>(request, cancellationToken).ConfigureAwait(false);
     }
 
     /// <inheritdoc />
@@ -123,6 +128,62 @@ public class GraphQLHttpClient : IGraphQLWebSocketClient, IDisposable
     public Task SendPongAsync(object? payload) => GraphQlHttpWebSocket.SendPongAsync(payload);
 
     #region Private Methods
+
+    private async Task<GraphQLHttpResponse<TResponse>> SendAPQHttpRequestAsync<TResponse>(GraphQLRequest request, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var savedQuery = request.Query;
+        bool useAPQ = false;
+
+        if (request.Query != null && _useAPQ && Options.EnableAutomaticPersistedQueries(request))
+        {
+            // https://www.apollographql.com/docs/react/api/link/persisted-queries/
+            const int APQ_SUPPORTED_VERSION = 1;
+            useAPQ = true;
+            // TODO: I suggest to change request.Extension type to public Dictionary<string, object?>?
+            //request.Extensions ??= new();
+            //request.Extensions["persistedQuery"] = new Dictionary<string, object>
+            //{
+            //    ["version"] = APQ_SUPPORTED_VERSION,
+            //    ["sha256Hash"] = Hash.Compute(request.Query),
+            //};
+            request.Extensions ??= new
+            {
+                version = APQ_SUPPORTED_VERSION,
+                sha256Hash = Hash.Compute(request.Query),
+            };
+            request.Query = null;
+        }
+
+        var response = await SendHttpRequestAsync<TResponse>(request, cancellationToken);
+
+        if (useAPQ && response.Errors?.Length > 0)
+        {
+            if (response.Errors.Any(error => string.Equals(error.Message, "PersistedQueryNotFound", StringComparison.CurrentCultureIgnoreCase)))
+            {
+                // GraphQL server supports APQ!
+
+                // Alas, for the first time we did not guess and in vain removed Query, so we return Query and
+                // send request again. This is one-time "cache miss", not so scary. By the way, after a second call,
+                // there is a probability of receiving PERSISTED_QUERY_NOT_FOUND error again, but it is so ghostly
+                // that this can and should be neglect so not to complicate the code without the need.
+                request.Query = savedQuery;
+                return await SendHttpRequestAsync<TResponse>(request, cancellationToken);
+            }
+            else if (response.Errors.Any(error => string.Equals(error.Message, "PersistedQueryNotSupported") ||
+                     response.Errors.Any(error => string.Equals(error.Message, "GraphQL query is missing.")))) // GraphQL.NET specific error message
+            {
+                // GraphQL server either supports APQ of some other version, or does not support it at all.
+                // Send a request for the second time. This is better than returning an error. Let the client work with APQ disabled.
+                _useAPQ = false;
+                request.Query = savedQuery;
+                return await SendHttpRequestAsync<TResponse>(request, cancellationToken);
+            }
+        }
+
+        return response;
+    }
 
     private async Task<GraphQLHttpResponse<TResponse>> SendHttpRequestAsync<TResponse>(GraphQLRequest request, CancellationToken cancellationToken = default)
     {
